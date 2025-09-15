d17d72072f2e876e7cc888cca70a2f4c15534c52
kees <me@keestucker.com>
2025-09-15 20:59:51 +1200

    feat: added full screen fuzzel config

--- scripts/wallust.py
+++ scripts/wallust.py
@@ -19,22 +19,22 @@ class WallustBase:
 
 
 class WallustDark(WallustBase):
-    crust      = "100C0E"
-    mantle     = "151013"
-    base       = "1A1418"
-    surface0   = "484346"
-    surface1   = "767274"
-    surface2   = "989597"
+    crust      = "0D0D10"
+    mantle     = "121215"
+    base       = "16161A"
+    surface0   = "454548"
+    surface1   = "737376"
+    surface2   = "969698"
 
-    text       = "EDDEEB"
-    subtext1   = "C9BDC8"
-    subtext0   = "A69BA5"
-    overlay2   = "6B646A"
-    overlay1   = "3B383B"
-    overlay0   = "242123"
+    text       = "B7DEE0"
+    subtext1   = "9CBDBE"
+    subtext0   = "809B9D"
+    overlay2   = "526465"
+    overlay1   = "2E3838"
+    overlay0   = "1B2122"
 
-    accent     = "5B58C0"
-    accent1     = "E24575"
+    accent     = "7E6386"
+    accent1     = "A2798D"
 
     red        = "fd5f51"
     green      = "38c76a"
@@ -43,22 +43,22 @@ class WallustDark(WallustBase):
 
 
 class WallustLight(WallustBase):
-    crust      = "8E858D"
-    mantle     = "BEB2BC"
-    base       = "EDDEEB"
-    surface0   = "F1E5EF"
-    surface1   = "F4EBF3"
-    surface2   = "F7F0F6"
+    crust      = "6E8586"
+    mantle     = "92B2B3"
+    base       = "B7DEE0"
+    surface0   = "C5E5E6"
+    surface1   = "D4EBEC"
+    surface2   = "DFF0F1"
 
-    text       = "1A1418"
-    subtext1   = "161114"
-    subtext0   = "120E11"
-    overlay2   = "0C090B"
-    overlay1   = "060506"
-    overlay0   = "040304"
+    text       = "16161A"
+    subtext1   = "131316"
+    subtext0   = "0F0F12"
+    overlay2   = "0A0A0C"
+    overlay1   = "050506"
+    overlay0   = "030304"
 
-    accent     = "53509E"
-    accent1     = "B9496C"
+    accent     = "6D5873"
+    accent1     = "8C6E7C"
 
     red        = "fd5f51"
     green      = "38c76a"
