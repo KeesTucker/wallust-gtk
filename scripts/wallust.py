7f25b817a5688f97db24bbf722452e1ed503a43f
kees <me@keestucker.com>
2025-09-17 08:58:55 +1200

    chore: set fuzzel to dpi aware no as it fucks with the scaling in PIP mode

--- scripts/wallust.py
+++ scripts/wallust.py
@@ -19,22 +19,22 @@ class WallustBase:
 
 
 class WallustDark(WallustBase):
-    crust      = "0D0D10"
-    mantle     = "121215"
-    base       = "16161A"
-    surface0   = "454548"
-    surface1   = "737376"
-    surface2   = "969698"
+    crust      = "080000"
+    mantle     = "0B0000"
+    base       = "0E0000"
+    surface0   = "3E3333"
+    surface1   = "6E6666"
+    surface2   = "938C8C"
 
-    text       = "B7DEE0"
-    subtext1   = "9CBDBE"
-    subtext0   = "809B9D"
-    overlay2   = "526465"
-    overlay1   = "2E3838"
-    overlay0   = "1B2122"
+    text       = "BDF7BC"
+    subtext1   = "A1D2A0"
+    subtext0   = "84AD84"
+    overlay2   = "556F55"
+    overlay1   = "2F3E2F"
+    overlay0   = "1C251C"
 
-    accent     = "7E6386"
-    accent1     = "A2798D"
+    accent     = "487F82"
+    accent1     = "7580A6"
 
     red        = "fd5f51"
     green      = "38c76a"
@@ -43,22 +43,22 @@ class WallustDark(WallustBase):
 
 
 class WallustLight(WallustBase):
-    crust      = "6E8586"
-    mantle     = "92B2B3"
-    base       = "B7DEE0"
-    surface0   = "C5E5E6"
-    surface1   = "D4EBEC"
-    surface2   = "DFF0F1"
+    crust      = "719471"
+    mantle     = "97C696"
+    base       = "BDF7BC"
+    surface0   = "CAF9C9"
+    surface1   = "D7FAD7"
+    surface2   = "E1FBE1"
 
-    text       = "16161A"
-    subtext1   = "131316"
-    subtext0   = "0F0F12"
-    overlay2   = "0A0A0C"
-    overlay1   = "050506"
-    overlay0   = "030304"
+    text       = "0E0000"
+    subtext1   = "0C0000"
+    subtext0   = "0A0000"
+    overlay2   = "060000"
+    overlay1   = "030000"
+    overlay0   = "020000"
 
-    accent     = "6D5873"
-    accent1     = "8C6E7C"
+    accent     = "446D70"
+    accent1     = "70778F"
 
     red        = "fd5f51"
     green      = "38c76a"
